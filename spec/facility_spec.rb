4c076ada78aeb7b15270f58f72d50f30cfafbd29
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-08-29 17:33:27 -0500

    test: added register vehicles to facility

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+    @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+    @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
+    @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
   end
   describe '#initialize' do
     it 'can initialize' do
@@ -11,6 +14,7 @@ RSpec.describe Facility do
       expect(@facility.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
       expect(@facility.phone).to eq('(720) 865-4600')
       expect(@facility.services).to eq([])
+      expect(@facility.registered_vehicles).to eq([])
     end
   end
 
@@ -21,6 +25,19 @@ RSpec.describe Facility do
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+
+  describe '#register vehicle' do
+    it 'can register a vehicle' do
+      @facility.register_vehicle(@cruz)
+      expect(@cruz.registration_date).to be_an_instance_of(Date)
+      expect(@facility.registered_vehicles.count).to eq(1)
+      @facility.register_vehicle(@bolt)
+      expect(@bolt.registration_date).to be_an_instance_of(Date)
+      @facility.register_vehicle(@camaro)
+      expect(@camaro.registration_date).to be_an_instance_of(Date)
+      expect(@facility.registered_vehicles.count).to eq(3)
     end
   end
 end
