d314e3eb7969bc00f243f6eae835d6ffa7122b6a
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-08-29 17:34:30 -0500

    feat: created ability to register vehicles & update registration date

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,23 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
+  attr_writer :registration_date
+
 
   def initialize(facility)
     @name = facility[:name]
     @address = facility[:address]
     @phone = facility[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+
+    vehicle.registration_date = Date.today
+    @registered_vehicles << vehicle
   end
 end
