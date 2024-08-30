f1e54688324e970a2944d22ae95c4796fa05013a
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-08-30 13:55:14 -0500

    feat: created method to collect fees and assign plates

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,6 +1,6 @@
 class Facility
-  attr_reader :name, :address, :phone, :services, :registered_vehicles
-  attr_writer :registration_date
+  attr_reader :name, :address, :phone, :services, :registered_vehicles, :collected_fees, :engine, :year
+  attr_writer :registration_date, :plate_type
 
 
   def initialize(facility)
@@ -9,6 +9,7 @@ class Facility
     @phone = facility[:phone]
     @services = []
     @registered_vehicles = []
+    @collected_fees = 0
   end
 
   def add_service(service)
@@ -17,10 +18,27 @@ class Facility
 
   def register_vehicle(vehicle)
     if @services.include?("Vehicle Registration") == true
+      collect_fee(vehicle)
       vehicle.registration_date = Date.today
       @registered_vehicles << vehicle
     else
       return 'This facility cannot register vehicles'
     end
   end
+
+  def collect_fee(vehicle)
+    if vehicle.antique? == true
+      @collected_fees += 25
+      vehicle.plate_type = :antique
+    elsif vehicle.engine == :ev
+      @collected_fees += 200
+      vehicle.plate_type = :ev
+    else
+      @collected_fees += 100
+      vehicle.plate_type = :regular
+    end
+  end
+
+  
+
 end
