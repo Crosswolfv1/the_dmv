a464f7d6c4121652306b116526d1bfe13dfadc6a
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-08-31 14:41:52 -0500

    tests: created tests for different data sources

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -15,8 +15,24 @@ RSpec.describe Facility_factory do
 
   describe '#create dmvs' do
     it 'can create dmvs' do
-      dmvs = @facility_factory.create_facilities(@colorado_facilities)
-      expect(dmvs).to all(be_an_instance_of(Facility))
+      facilities = @facility_factory.create_facilities(@colorado_facilities)
+      expect(facilities).to all(be_an_instance_of(Facility))
+      expect(facilities[0].name).to eq("DMV Tremont Branch")
+      expect(facilities[1].address).to eq("4685 Peoria Street Suite 101 Denver CO 80239")
+      expect(facilities[2].phone).to eq("(720) 865-4600")
+    end
+
+    it 'can create dmvs from multiple data sources' do
+      facilities1 = @facility_factory.create_facilities(@new_york_facilities)
+      expect(facilities1[0].name).to eq()
+      expect(facilities1[1].address).to eq()
+      expect(facilities1[2].phone).to eq()
+
+      facilities2 = @facility_factory.create_facilities(@missouri_facilities)
+      expect(facilities2[0].name).to eq()
+      expect(facilities2[1].address).to eq()
+      expect(facilities2[2].phone).to eq()
+
     end
   end
 
