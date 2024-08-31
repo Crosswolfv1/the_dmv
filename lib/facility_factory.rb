ceda13cdabdbf9028c8b43cdb275f7ab98947056
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-08-31 14:33:59 -0500

    bug: fixed code returing the wron address

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -4,11 +4,11 @@ class Facility_factory
 
   def create_facilities(source)
     source.map do |facility|
-      address_combined = ("#{facility['address_li']}  #{facility['address__1']}  #{facility['city']}  #{facility['state']}  #{facility['zip']}")
+      address_combined = ("#{facility[:address_li]} #{facility[:address__1]} #{facility[:city]} #{facility[:state]} #{facility[:zip]}")
       Facility.new({
-        name: facility['dmv_office'],
+        name: facility[:dmv_office],
         address: address_combined,
-        phone: facility['phone']
+        phone: facility[:phone]
       })
     end
   end
