eb01bb6aa14dcb9d462f4ad9cfbeecb3380f43de
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-08-31 14:01:35 -0500

    feat: built initial facility factory

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,12 +1,17 @@
-require './lib/dmv.rb'
+require './lib/facility.rb'
 
-class Facilityacility_factory
+class Facility_factory
 
   def create_dmvs(source)
-      Dmv.new({
-        
+    source.map do |facility|
+      address_combined = (facility['address_li'] + ' ' + facility['address__1'] + ' ' + facility['city'] + ' ' + facility['state'] + ' ' + facility['zip']).to_s
+      Facility.new({
+        name: facility['dmv_office'],
+        address: address_combined,
+        phone: facility['phone']
       })
     end
+    
   end
 
 end
