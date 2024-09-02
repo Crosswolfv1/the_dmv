ffeb00abf0ed5b742be8732e72955da7d597cda0
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-02 17:30:52 -0500

    test: created tests to verify fcatory creates unique objects

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -17,6 +17,13 @@ RSpec.describe Vehicle_factory do
       vehicles = @factory.create_vehicles(@wa_ev_registrations)
       expect(vehicles).to all(be_an_instance_of(Vehicle))
     end
+
+    it 'can create unique vehicles' do
+      vehicles = @factory.create_vehicles(@wa_ev_registrations)
+      expect(vehicles[0].engine).to eq :ev
+      expect(vehicles[1].make).to eq "BMW"
+      expect(vehicles[2].model).to eq "Mustang Mach-E"
+    end
   end
 
 end
