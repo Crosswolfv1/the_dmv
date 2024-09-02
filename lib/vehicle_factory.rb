083e16a8e3702875d6c70479e80384ddf2ec6485
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-02 16:49:53 -0500

    codes written to pull data from API, one test created

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,1 +1,19 @@
 class VehicleFactory
+    
+
+    def initialize 
+    end
+
+    def create_vehicles(source)
+        vehicles = []
+        source.each do |data|
+        vehicles << create_vehicle(data)
+        end 
+        vehicles 
+    end
+
+     def create_vehicle(data)
+        Vehicle.new(data)
+    end
+
+end
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -1,4 +1,18 @@
 require './lib/dmv_data_service'
 require './lib/vehicle_factory'
 require './lib/vehicle'
-Factory = VehicleFactory.new
+
+
+factory = VehicleFactory.new
+
+RSpec.describe VehicleFactory do
+describe 'getting information' do
+    it 'holds an array' do
+
+    vehicles = VehicleFactory.new
+ 
+    expect(vehicles).to be_an_instance_of(VehicleFactory)
+    require 'pry'; binding.pry
+    end
+  end
+end
