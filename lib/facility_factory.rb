7e65a8f1fbb76c161ea84cd03a3d37dff120a075
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-02 18:57:36 -0500

    feat, added ability tro see what facility has what services

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -3,13 +3,16 @@ require './lib/facility.rb'
 class Facility_factory
 
   def create_facilities(source)
-    source.map do |facility|
-      Facility.new({
-        name: facility[:dmv_office] || facility[:office_name] || facility[:name],
-        address: address_formatted(facility),
-        phone: facility[:phone] || facility[:public_phone_number]
+    source.map do |facility_data|
+      facility = Facility.new({
+        name: facility_data[:dmv_office] || facility_data[:office_name] || facility_data[:name],
+        address: address_formatted(facility_data),
+        phone: facility_data[:phone] || facility_data[:public_phone_number],
       })
-    end
+      add_services(facility, facility_data[:services_p])
+      
+      facility
+      end
   end
 
   def address_formatted(facility)
@@ -25,4 +28,18 @@ class Facility_factory
     end.map { |key| facility[key] }
     address_parts.compact.join(' ')
   end
+
+  def add_services(facility, services_string)
+    return unless services_string
+  
+    services = services_string.split(',').map(&:strip)
+    services.each do |service|
+      facility.add_service(service)
+    end
+  end
+  
+
+
 end
+
+"vehicle titles, registration, renewals;  VIN inspections"
