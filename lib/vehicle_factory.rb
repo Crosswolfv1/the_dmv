0c7b4ab337115398223bd9ef9ac51da40d66c3b1
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-03 14:54:27 -0500

    changed the format of code

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,19 +1,24 @@
 class VehicleFactory
     
 
-    def initialize 
-    end
+ 
 
     def create_vehicles(source)
         vehicles = []
         source.each do |data|
-        vehicles << create_vehicle(data)
+           vehicle = Vehicle.new({
+                :vin => data[:vin_1_10],
+                :make => data[:make],
+                :model => data[:model],
+                :year => data[:model_year].to_i,
+                :engine => :ev
+            })
+        vehicles << vehicle
         end 
-        vehicles 
-    end
-
-     def create_vehicle(data)
-        Vehicle.new(data)
+        vehicles
     end
 
 end
+
+
+
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -6,13 +6,20 @@ require './lib/vehicle'
 factory = VehicleFactory.new
 
 RSpec.describe VehicleFactory do
-describe 'getting information' do
-    it 'holds an array' do
+ it 'is an instance of the vehicle factory' do
 
     vehicles = VehicleFactory.new
  
     expect(vehicles).to be_an_instance_of(VehicleFactory)
-    require 'pry'; binding.pry
-    end
+    #require 'pry'; binding.pry
+  end
+  it 'creates new vehicles based from an external data set' do
+    vehicles = VehicleFactory.new
+    source = DmvDataService.new.wa_ev_registrations
+
+    vehicle.create_vehicles(source)
+
+    expect(vehicle.create_vehicles(source)).to eq
   end
 end
+
