84d5da09320f2826b7d33df0f6ed7c81bcd3ad4f
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-03 14:56:13 -0500

    added code to access CO DMV locations info

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,17 +1,21 @@
 class FacilityFactory
 
-    def initialize
-    end
-
-    def create_facilities(source)
+    
+    
+    
+    def co_facilities(source)
         facilities = []
         source.each do |data|
-        facilities << create_facility(data)
+            address = "#{data[:address_li]}, #{data[:address__1]}, #{data[:city]}, #{data[:state]} #{data[:zip]}"
+           facility = Facility.new({
+                :name => data[:dmv_office],
+                :address => address,
+                :phone => data[:phone],
+                :services => data[:services_p]
+            })
+        facilities << facility
         end 
-        facilities 
+        facilities
     end
-    
-     def create_facility(data)
-        Facility.new(data)
-    end
+
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -1,2 +1,16 @@
-require './spec_helper'
+require 'spec_helper'
 
+
+RSpec.describe FacilityFactory do
+
+    before(:each) do
+        facilities = FacilityFactory.new
+        co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+        source = DmvDataService.new.co_dmv_office_locations 
+
+        facilities.co_facilities(source)
+       
+    
+    #     end
+    # end
+end
