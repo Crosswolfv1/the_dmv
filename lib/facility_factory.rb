eee60a943357307ae97227573e9a165882af7e4a
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-03 16:39:25 -0500

    added ny locations and adjacent tests

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -2,7 +2,7 @@ class FacilityFactory
 
     
     
-    
+
     def co_facilities(source)
         facilities = []
         source.each do |data|
@@ -18,4 +18,34 @@ class FacilityFactory
         facilities
     end
 
+
+    def ny_facilities(source)
+        facilities = []
+        source.each do |data|
+            address = "#{data[:street_address_line_1]}, #{data[:city]}, #{data[:state]} #{data[:zip_code]}"
+           facility = Facility.new({
+                :name => data[:office_name],
+                :address => address,
+                :phone => data[:public_phone_number]
+            })
+        facilities << facility
+        end 
+        facilities
+    end
+
+    def ny_facilities(source)
+        facilities = []
+        source.each do |data|
+            address = "#{data[:street_address_line_1]}, #{data[:city]}, #{data[:state]} #{data[:zip_code]}"
+           facility = Facility.new({
+                :name => data[:office_name],
+                :address => address,
+                :phone => data[:public_phone_number]
+            })
+        facilities << facility
+        end 
+        facilities
+    end
+
+    
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -2,15 +2,56 @@ require 'spec_helper'
 
 
 RSpec.describe FacilityFactory do
-
     before(:each) do
-        facilities = FacilityFactory.new
-        co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
-        source = DmvDataService.new.co_dmv_office_locations 
-
-        facilities.co_facilities(source)
+        @facilities = FacilityFactory.new
+        DmvDataService.new.co_dmv_office_locations
+        @co_source = DmvDataService.new.co_dmv_office_locations 
+        @ny_source = DmvDataService.new.ny_dmv_office_locations
+        @mis_source = DmvDataService.new.mo_dmv_office_locations
+    # facilities.co_facilities(source)
+    end  
+    '#describe colorado location attributes' do
+    it 'can count the number of locations in the array' do
        
-    
-    #     end
-    # end
+        expect(@facilities.co_facilities(@co_source).count).to eq(5)
+    end
+
+   it 'can give the name of the last location' do
+        expect(@facilities.co_facilities(@co_source).last.name).to eq("DMV Southeast Branch")
+    end
+
+    it 'can give the phone number of the 1st index of the array' do
+        expect(@facilities.co_facilities(@co_source)[1].phone).to eq("(720) 865-4600")
+    end
+
+    it 'can give the address of the 4th index of the array' do
+        expect(@facilities.co_facilities(@co_source)[4].address).to eq("2243 S Monaco Street Pkwy, , Denver, CO 80222")
+    end
+
+    it 'can give the name of the location in the 3rd index of the array' do
+        expect(@facilities.co_facilities(@co_source)[0].name).to eq("DMV Tremont Branch")
+    end
+end
+
+    '#describe ny location attributes' do
+    it 'can give the name of index 0 in the array' do
+
+        expect(@facilities.ny_facilities(@ny_source)[0].name).to eq("HUDSON")
+    end
+
+    it 'can give the phone number of the last location' do
+        expect(@facilities.ny_facilities(@ny_source).last.phone).to eq("7189666155")
+    end
+
+    it 'can give the address of the location in index 5 in the array' do
+        expect(@facilities.ny_facilities(@ny_source)[5].address).to eq("1 LARKIN PLAZA, YONKERS, NY 10701")
+    end
+
+    it 'can count the number of locations in the array' do
+        expect(@facilities.ny_facilities(@ny_source).count).to eq(172)
+
+    it 'can give the name of the location in index 99 in the array' do
+        expect(@facilities.ny_facilities(@ny_source)[99].name).to eq("DELHI")
+    end
+end
 end
