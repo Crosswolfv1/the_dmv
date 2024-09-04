ddf9a915abb06d90562e8f3eae46c098652bf358
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-03 20:34:50 -0500

    cleaned up codes and tests, ensured tests passing

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -3,21 +3,20 @@ class VehicleFactory
 
  
 
-    def create_vehicles(source)
-        vehicles = []
-        source.each do |data|
-           vehicle = Vehicle.new({
-                :vin => data[:vin_1_10],
-                :make => data[:make],
-                :model => data[:model],
-                :year => data[:model_year].to_i,
-                :engine => :ev
+def create_vehicles(source)
+    vehicles = []
+    source.each do |data|
+        vehicle = Vehicle.new({
+            :vin => data[:vin_1_10],
+            :make => data[:make],
+            :model => data[:model],
+            :year => data[:model_year].to_i,
+            :engine => :ev
             })
         vehicles << vehicle
         end 
         vehicles
     end
-
 end
 
 
--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -12,27 +12,33 @@ RSpec.describe VehicleFactory do
         @source = DmvDataService.new.wa_ev_registrations
     end
     it 'is has vehicles as an instance of the vehicle factory' do 
+
         expect(@vehicles).to be_an_instance_of(VehicleFactory)
-  end
+    end
 
     it 'can count the number of vehicles' do
+
         expect(@vehicles.create_vehicles(@source).count).to eq(1000)
-  end
+    end
 
     it 'can will return an array of vehicles' do
+
         expect(@vehicles.create_vehicles(@source)).to be_an(Array)
     end
 
     it 'can give the make of the first vehicle' do
+
         expect(@vehicles.create_vehicles(@source).first.make).to eq("TESLA")
     end
 
     it 'can give the make and the model of the vehicle in index 45' do
+
         expect(@vehicles.create_vehicles(@source)[45].model).to eq("X3")
         expect(@vehicles.create_vehicles(@source)[45].make).to eq("BMW")
     end
 
     it 'can give the year of the last vehicle' do
+        
         expect(@vehicles.create_vehicles(@source).last.year).to eq(2015)
     end
 end
