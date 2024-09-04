747a86cd07038c9a575ffdc3099f548c88786982
Natasha Vasquez <nvasquez9020@my.pmi.edu>
2024-09-03 20:33:36 -0500

    cleand up codes and tests, ensured tests passing

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -23,65 +23,74 @@ RSpec.describe Registrant do
     end
 
     it 'can give registrant information' do
+
     expect(registrant_1.name).to eq("Bruce")
     expect(registrant_2.name).to eq("Penny")
 
     expect(registrant_1.age).to eq(18)
     expect(registrant_2.age).to eq(16)
     end
-    #require 'pry'; binding.pry
 
     it 'can give permit information' do
+
         expect(registrant_1.permit?).to eq(true)
         expect(registrant_2.permit?).to eq(false)
     end
 
     it 'can give license data' do
+
         expect(registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
 
     it 'can have a registrant earn a permit' do
         registrant_2.earn_permit
-         expect(registrant_2.permit?).to eq(true)
+
+        expect(registrant_2.permit?).to eq(true)
     end
 
-    describe '#getting a written test' do
+describe '#getting a written test' do
     it 'can update license data' do
+
         expect(registrant_2.permit?).to eq(false)
         expect(registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
         expect(registrant_2.earn_permit).to eq(true)
         expect(registrant_2.permit?).to eq(true)
         expect(registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
     end
-    end
-    #require 'pry'; binding.pry
-    describe '#getting a license' do
+end
+   
+describe '#getting a license' do
     it 'can update the license' do
         registrant_1.earn_permit
         registrant_1.administer_road_test
-       expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
-       
-    end
-    end
 
-    describe '#renewing a license' do
+       expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false}) 
+    end
+end
+
+describe '#renewing a license' do
     it 'can renew a license' do
         registrant_1.earn_permit
         registrant_1.administer_road_test
         registrant_1.renew_drivers_license
-        require 'pry'; binding.pry
+        
         expect(registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
     end
-    end
+end
 
-    describe '#knowing if criteria is met for tests' do
+describe '#knowing if criteria is met for tests' do
     it 'can decide if a registrant meets the age requirement for license tests' do
         registrant_3.earn_permit
+
         expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        
         registrant_3.administer_road_test
+
         expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+        
         registrant_3.renew_drivers_license
+
         expect(registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
-    end
+end
 end
